75f4bebbd65e7b5ad25c5029f8c1a11327192a1e
aimG313 <ashrafulmahi313@gmail.com>
2025-04-01 22:35:03 +0600

    Update Project_1_Arduino_Board_Cleaning.ino

--- Project_1_Arduino_Board_Cleaning/Project_1_Arduino_Board_Cleaning.ino
+++ Project_1_Arduino_Board_Cleaning/Project_1_Arduino_Board_Cleaning.ino
@@ -1,13 +1,10 @@
-int pin = 13, d = 3000;
+
 void setup()
 {
-  pinMode(pin, OUTPUT);
+ 
 }
 
 void loop() 
 {
-  digitalWrite(pin, HIGH);
-  delay(d);
-  digitalWrite(pin, LOW);
-  delay(d);
+  
 }
